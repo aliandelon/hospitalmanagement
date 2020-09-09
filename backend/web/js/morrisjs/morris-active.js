9ff871b96d436b9142a0b3f16a178685ad3286f0
9605097360 <ebypaul.t@gmail.com>
2020-09-09 22:51:55 +0530

    Add Changes

--- backend/web/js/morrisjs/morris-active.js
+++ backend/web/js/morrisjs/morris-active.js
@@ -4,52 +4,38 @@ Morris.Area({
         element: 'morris-area-chart',
         data: [{
             period: '2012',
-            Python: 0,
-            PHP: 0,
-            Java: 0
+            Investigation: 60
         }, {
             period: '2013',
-            Python: 100,
-            PHP: 80,
-            Java: 65
+            Investigation: 100
         }, {
             period: '2014',
-            Python: 180,
-            PHP: 150,
-            Java: 120
+            Investigation: 180
         }, {
             period: '2015',
-            Python: 100,
-            PHP: 70,
-            Java: 40
+            Investigation: 100
         }, {
             period: '2016',
-            Python: 180,
-            PHP: 150,
-            Java: 120
+            Investigation: 180
         }, {
             period: '2017',
-            Python: 100,
-            PHP: 70,
-            Java: 40
+            Investigation: 100
         },
          {
             period: '2018',
-            Python: 180,
-            PHP: 150,
-            Java: 120
+            Investigation: 180
         }],
         xkey: 'period',
-        ykeys: ['Python', 'PHP', 'Java'],
-        labels: ['Python', 'PHP', 'Java'],
+        ykeys: ['Investigation'],
+        labels: ['Investigation'],
         pointSize: 0,
         fillOpacity: 0.99,
-        pointStrokeColors:['#65b12d', '#933EC5 ', '#006DF0'],
+        pointStrokeColors:['#05ab9e'],
         behaveLikeLine: true,
         gridLineColor: '#e0e0e0',
         lineWidth:0,
         hideHover: 'auto',
-        lineColors: ['#65b12d', '#933EC5 ', '#006DF0'],
+        lineColors: ['#05ab9e'],
         resize: true
         
     });
