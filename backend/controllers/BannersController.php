df42ee399a82fbaa1c61f9dd84e5e8f51481ba0e
9605097360 <ebypaul.t@gmail.com>
2020-09-11 02:04:26 +0530

    Add New files

---
+++ backend/controllers/BannersController.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use common\models\Banners;
+use common\models\BannersSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
+
+/**
+ * BannersController implements the CRUD actions for Banners model.
+ */
+class BannersController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Banners models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new BannersSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Banners model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Banners model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Banners();
+
+        // if ($model->load(Yii::$app->request->post()) && $model->save()) {
+        //     return $this->redirect(['view', 'id' => $model->id]);
+        // } else {
+        //     return $this->render('create', [
+        //         'model' => $model,
+        //     ]);
+        // }
+        $model->scenario='oncreate';
+        if ($model->load(Yii::$app->request->post()))
+            {
+                $file = UploadedFile::getInstance($model, 'image');
+                if ($file) {
+                   $model->image = $file->extension;     
+                }
+                if($model->save()) {
+                    if ($file) {
+                    $model->upload($file, $model->id, "banner".$model->id);
+                    }
+                return $this->redirect(['index']);
+                }
+            }
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+    }
+
+    /**
+     * Updates an existing Banners model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        $model->scenario='onupdate';
+        $images = $model->image;
+        if ($model->load(Yii::$app->request->post()))
+            {
+                
+                $file = UploadedFile::getInstance($model, 'image');
+                if($file != "") {
+                   $model->image = $file->extension;     
+                }else{
+                    $model->image = $images;
+                }
+                if($model->save()) {
+                    if ($file) {
+                    $model->upload($file, $model->id, "banner".$model->id);
+                    }
+                return $this->redirect(['index']);
+                }
+            }else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Banners model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Banners model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Banners the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Banners::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
