2ead8d184d8621c6c12478d009d359e9fdbe72f1
9605097360 <ebypaul.t@gmail.com>
2020-09-11 20:00:10 +0530

    Add Changes

--- backend/views/banners/index.php
+++ backend/views/banners/index.php
@@ -4,35 +4,88 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
-/* @var $searchModel common\models\BannersSearch */
+/* @var $searchModel common\models\GallerySearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Banners';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="banners-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+<div class="right_col" role="main">
+    <div class="">
+        <div class="page-title">
+            <div class="title_left">
+                <h3><?= Html::encode($this->title) ?> </h3>
+            </div>
 
-    <p>
-        <?= Html::a('Create Banners', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+            <div class="title_right">
+
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="x_panel">
+                    <div class="x_title">
+                        <ul class="nav navbar-right panel_toolbox">
+                                                                                                <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+                            
+                            <?= Html::a('Create Banner', ['create'], ['class' => 'btn btn-success']) ?>
+
+
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+
+                                                        <?= GridView::widget([
+                                'dataProvider' => $dataProvider,
+                                'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                                ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
-            'name',
-            'expiry_date',
-            'image:ntext',
-            'hospital_clinic_id',
+                                            // 'id',
+                                'name',
+            [
+                                    
+                                    'attribute' => 'image',
+                                    'value' => function($model) {
+
+    if ($model->image != '' && $model->id != "") {
+         return '<img width="100" style="border: 2px solid #d2d2d2;margin-right:.5em;" src="' . Yii::$app->request->baseUrl . '/../uploads/banners/'. $model->id.'/banner'.$model->id.'.'.$model->image . '" />';
+                                           
+                                        }else{
+                                            return '';
+                                        }
+                                    },
+                                    'format'=>'raw',
+
+        
+                                    ],
             // 'sort_order',
-            // 'status',
+            ['attribute' => 'status',
+                                    // 'filter'=>['1'=>'Enabled','0'=>'Disabled'],
+                                    'value' => function($data) {
+                                        if ($data->status == "1") {
+                                            return "Enabled";
+                                        } else {
+                                            return "Disabled";
+                                        }
+                                    }
+                                ],
 
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+
+                                ['class' => 'yii\grid\ActionColumn',
+                                    'header' => 'Update',
+                                    'template' => '{update}'],
+                                ['class' => 'yii\grid\ActionColumn',
+                                    'header' => 'Delete',
+                                    'template' => '{delete}'],
+                                ],
+                                ]); ?>
+                                                                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
