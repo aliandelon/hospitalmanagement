93c35c3c481b114f2143bed7b85eb5d348473ff0
9605097360 <ebypaul.t@gmail.com>
2020-09-13 21:14:28 +0530

    Add Changes

--- common/models/AdminDetailsSearch.php
+++ common/models/AdminDetailsSearch.php
@@ -67,7 +67,50 @@ class AdminDetailsSearch extends AdminDetails
         $query->andFilterWhere(['like', 'name', $this->name])
             ->andFilterWhere(['like', 'email', $this->email])
             ->andFilterWhere(['like', 'phone_number', $this->phone_number])
-            ->andFilterWhere(['like', 'address', $this->address]);
+            ->andFilterWhere(['like', 'address', $this->address])
+            ->andFilterWhere(['=', 'role_id', '1']);
+
+        return $dataProvider;
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search2($params)
+    {
+        $query = AdminDetails::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'admin_id' => $this->admin_id,
+            'role_id' => $this->role_id,
+            'status' => $this->status,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['like', 'phone_number', $this->phone_number])
+            ->andFilterWhere(['like', 'address', $this->address])
+            ->andFilterWhere(['=', 'role_id', '10']);
 
         return $dataProvider;
     }
