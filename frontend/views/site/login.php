055b614a69ecb0ff920d3dce747ff546b22d20fa
9605097360 <ebypaul.t@gmail.com>
2020-09-17 00:42:48 +0530

    Add changes

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -10,52 +10,55 @@ use yii\helpers\Url;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<section class="login">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <nav class="cl-effect-5">
-                    <a href="#"><span data-hover="Login">Login</span></a>
 
-                </nav>
-                <?php if (Yii::$app->session->hasFlash('register')): ?>
-                        <div class="alert alert-success alert-dismissable" style="text-align: center;">
-                            <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
-                            <h4><i class="icon fa fa-check"></i>Registered !</h4>
-                            <?= Yii::$app->session->getFlash('register') ?>
-                        </div>
-                <?php endif; ?>
-                <?php if (Yii::$app->session->hasFlash('activation')): ?>
-                        <div class="alert alert-success alert-dismissable" style="text-align: center;">
-                            <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
-                            <h4><i class="icon fa fa-check"></i>Activated !</h4>
-                            <?= Yii::$app->session->getFlash('activation') ?>
-                        </div>
-                <?php endif; ?>
-                <img class="center-block dots" src="<?= Url::base() . '/images/dots.png' ?>">
-                <div class="new_form loginMod">
-                    <?php $form = ActiveForm::begin(['id' => 'login-form', 'options' => ['role' => 'form']]); ?>
+          <?php 
+          $form = ActiveForm::begin(
+                [
+                    
+                    'options' => [
+                        'class' => 'login100-form validate-form'
+                     ]
+                ]
+            ); 
+            ?>
+                    <!-- <form class="login100-form validate-form"> -->
+					<span class="login100-form-logo">
+						<!-- <i class="zmdi zmdi-landscape"></i> -->
+						<img src="<?= Yii::$app->request->baseUrl; ?>/login/images/logosn.png" width="50%">
+					</span>
 
-                    <div class="form-group">
-                        <?= $form->field($model, 'username')->input('email', ['autofocus' => true, 'class' => 'form-new', 'placeholder' => 'Email'])->label(false) ?>
-                    </div>
-                    <div class="form-group">
-                        <?= $form->field($model, 'password')->passwordInput(['autofocus' => true, 'class' => 'form-new', 'placeholder' => 'Password'])->label(false)->label(false) ?>
-                    </div>
+					<div class="wrap-input100 validate-input" data-validate = "Enter username">
+						<input class="input100" type="text" name="LoginForm[username]" placeholder="Email">
+						<span class="focus-input100" data-placeholder="&#xf207;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" type="password" name="LoginForm[username]" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xf191;"></span>
+					</div>
 
 
-                    <?= Html::submitButton('Login', ['class' => 'btn new-btn btn-default', 'name' => 'login-button']) ?>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
 
-                    <?php ActiveForm::end(); ?>
-                    <div class="col-md-5 col-sm-5 col-xs-5 padd">
-                        <a class="forgot" href="<?php echo Yii::$app->getUrlManager()->createUrl('site/register'); ?>">New User?</a>
-                    </div>
-                    <div class="col-md-7 col-sm-7 col-xs-7 padd">
-                        <a class="new" href="<?php echo Yii::$app->getUrlManager()->createUrl('site/request-password-reset'); ?>"">Forgot your password</a>
+					<!-- <div class="text-center p-t-50">
+						<a class="txt1" href="#">
+							Forgot Password?
+						</a>
+					</div> -->
+				<!-- </form> -->
+                
+        <?php ActiveForm::end(); ?>
 
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+
+        
+
+
+
+
+
+
+
