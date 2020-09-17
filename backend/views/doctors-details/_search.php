c590d66cc14404ed30f46ac6f7a37aec4c40afee
9605097360 <ebypaul.t@gmail.com>
2020-09-18 00:43:29 +0530

    Add Cahnges

--- backend/views/doctors-details/_search.php
+++ backend/views/doctors-details/_search.php
@@ -25,23 +25,23 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'phone') ?>
 
-    <?php // echo $form->field($model, 'gender') ?>
+    <?php echo $form->field($model, 'gender') ?>
 
-    <?php // echo $form->field($model, 'registration_no') ?>
+    <?php echo $form->field($model, 'registration_no') ?>
 
-    <?php // echo $form->field($model, 'experience') ?>
+    <?php echo $form->field($model, 'experience') ?>
 
-    <?php // echo $form->field($model, 'profile_image') ?>
+    <?php echo $form->field($model, 'profile_image') ?>
 
-    <?php // echo $form->field($model, 'specialty_id') ?>
+    <?php echo $form->field($model, 'specialty_id') ?>
 
-    <?php // echo $form->field($model, 'qualifications') ?>
+    <?php echo $form->field($model, 'qualifications') ?>
 
-    <?php // echo $form->field($model, 'address') ?>
+    <?php echo $form->field($model, 'address') ?>
 
-    <?php // echo $form->field($model, 'status') ?>
+    <?php echo $form->field($model, 'status') ?>
 
-    <?php // echo $form->field($model, 'created_on') ?>
+    <?php echo $form->field($model, 'created_on') ?>
 
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
