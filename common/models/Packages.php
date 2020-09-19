b8448c84195c82adce109245816aec4184511d96
9605097360 <ebypaul.t@gmail.com>
2020-09-19 16:02:34 +0530

    Add

---
+++ common/models/Packages.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "packages".
+ *
+ * @property integer $id
+ * @property string $package_name
+ * @property string $price
+ * @property string $description
+ * @property string $validity
+ * @property integer $sort_order
+ * @property integer $status
+ * @property string $created_on
+ * @property string $updated_on
+ */
+class Packages extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'packages';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['package_name', 'price', 'description', 'validity', 'sort_order', 'status'], 'required'],
+            [['price'], 'number'],
+            [['description'], 'string'],
+            [['validity', 'created_on', 'updated_on'], 'safe'],
+            [['sort_order', 'status','validity'], 'integer'],
+            [['package_name'], 'string', 'max' => 150],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'package_name' => 'Package Name',
+            'price' => 'Price',
+            'description' => 'Description',
+            'validity' => 'Validity (in Days)',
+            'sort_order' => 'Sort Order',
+            'status' => 'Status',
+            'created_on' => 'Created On',
+            'updated_on' => 'Updated On',
+        ];
+    }
+}
