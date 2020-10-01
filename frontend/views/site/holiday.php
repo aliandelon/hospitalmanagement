49aa65809b12be737a22ed3bcc9c31d22938baba
9605097360 <ebypaul.t@gmail.com>
2020-10-02 01:35:50 +0530

    Add Changes

--- frontend/views/site/holiday.php
+++ frontend/views/site/holiday.php
@@ -119,13 +119,7 @@ div .bootstrap-select{
 }
 
 </style>
-<head>
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
-  </head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
+
 
 <script type="text/javascript">
     var defaultEvents;
@@ -138,8 +132,8 @@ div .bootstrap-select{
 <?php
 $this->registerJs("
         $(document).ready(function(){     
-        // $('#Investigation').selectpicker();
-        // $('#doctor').selectpicker();
+        $('#Investigation').selectpicker();
+        $('#doctor').selectpicker();
                 // $(function() {
                  
                 // });
