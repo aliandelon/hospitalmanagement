ba18c1887859a8f3af4849785672dc187d3430cb
9605097360 <ebypaul.t@gmail.com>
2020-10-05 01:24:37 +0530

    Add Changes

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -11,6 +11,11 @@ use frontend\models\Students;
 use backend\models\Topics;
 use backend\models\Posts;
 use backend\models\Forms;
+use common\models\Login;
+use common\models\DoctorsDetails;
+use common\models\Appointments;
+use common\models\Investigations;
+use common\models\Banners;
 
 /**
  * Site controller
@@ -62,7 +67,27 @@ class SiteController extends Controller {
          * @return string
          */
         public function actionIndex() {
-        	$params = [];
+            $params = [];
+            $model = new Login();
+            $model1 = new DoctorsDetails();
+            $model2 = new Appointments();
+            $model3 = new Investigations();
+            $model4 = new Banners();
+            $first_day_this_month = date('Y-m-01'); // hard-coded '01' for first day
+            $last_day_this_month  = date('Y-m-t');
+            $params['registeredHospital'] = $model->getUserCountBasedOnTypes('3');
+            $params['registeredSubAdmins'] = $model->getUserCountBasedOnTypes('2');
+            $params['registeredDoctors'] = $model1->getDocCount();
+            $params['registeredPatients'] =  $model->getUserCountBasedOnTypes('4');
+            $params['totalAppointments'] =  $model2->getAppointmentCount();
+            $params['totalInvestigations'] =  $model3->getInvestigationCount();
+            $params['totalInvestigationsMonthwise'] =  $model3->getInvestigationMonthwiseCount();
+            $params['totalDocAppointmentsMonthwise'] = $model1->getDoctorMonthwiseCount();
+            $params['totalBanners'] =  $model4->getBannerCount();
+            $params['activeHospital'] = $model->getUserCountBasedOnTypes('3','1');
+            $params['topRatedHospital'] = $model2->getTopRatedHospitals($first_day_this_month,$last_day_this_month);
+            $params['topRatedDoctors'] = $model2->getTopRatedDoctors($first_day_this_month,$last_day_this_month);
+            $params['topRatedInvestigation'] = $model2->getTopRatedInvestigations($first_day_this_month,$last_day_this_month);
         	return $this->render('index',['params'=>$params]);
         }
 
