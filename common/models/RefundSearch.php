3c5720ea086257e47cdb31f07e1623bed5e390c3
InboxReacher <donboscokunnath@gmail.com>
2020-12-12 18:22:12 +0530

    changes added

--- common/models/RefundSearch.php
+++ common/models/RefundSearch.php
@@ -78,4 +78,87 @@ class RefundSearch extends Refund
 
         return $dataProvider;
     }
+     public function rejectrefund($params)
+    {
+        $query = Refund::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'booking_id' => $this->booking_id,
+            'patient_id' => $this->patient_id,
+            'doctor_id' => $this->doctor_id,
+            'investigation_id' => $this->investigation_id,
+            'hospital_clinic_id' => $this->hospital_clinic_id,
+            'app_date' => $this->app_date,
+            'app_time' => $this->app_time,
+            'price' => $this->price,
+            'status' => $this->status,
+        ]);
+
+        $query->andFilterWhere(['like', 'razorpay_refund_id', $this->razorpay_refund_id])
+            ->andFilterWhere(['like', 'razorpay_payment_id', $this->razorpay_payment_id])
+            ->andFilterWhere(['like', 'razorpay_order_id', $this->razorpay_order_id]);
+        $query->andFilterWhere(['=', 'status', 1]);
+
+        return $dataProvider;
+    }
+
+
+
+      public function approverefund($params)
+    {
+        $query = Refund::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'booking_id' => $this->booking_id,
+            'patient_id' => $this->patient_id,
+            'doctor_id' => $this->doctor_id,
+            'investigation_id' => $this->investigation_id,
+            'hospital_clinic_id' => $this->hospital_clinic_id,
+            'app_date' => $this->app_date,
+            'app_time' => $this->app_time,
+            'price' => $this->price,
+            'status' => $this->status,
+        ]);
+
+        $query->andFilterWhere(['like', 'razorpay_refund_id', $this->razorpay_refund_id])
+            ->andFilterWhere(['like', 'razorpay_payment_id', $this->razorpay_payment_id])
+            ->andFilterWhere(['like', 'razorpay_order_id', $this->razorpay_order_id]);
+        $query->andFilterWhere(['=', 'status', 2]);
+
+        return $dataProvider;
+    }
+
+    
 }
